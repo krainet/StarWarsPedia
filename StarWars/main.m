fafc995d9cf00f7ce83b56c52d20cee05aa73847
RAMON ALBERTI DANES <firebird@MacBook-Pro-de-RAMON.local>
2015-03-24 21:43:50 +0100

    Initial Commit

---
+++ StarWars/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StarWars
+//
+//  Created by RAMON ALBERTI DANES on 24/3/15.
+//  Copyright (c) 2015 Krainet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
